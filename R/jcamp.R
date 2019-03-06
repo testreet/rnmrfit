291c5a391d3c0650947f9014dc0c4aa095b877e9
Stanislav Sokolenko <stanislav@sokolenko.net>
2019-03-06 16:47:57 -0400

    Fixed read_jcamp() to consider XYDATA=(X++(Y..Y)) type entries.

--- R/jcamp.R
+++ R/jcamp.R
@@ -521,12 +521,109 @@ process_jcamp_entry <- function(jcamp.entry, sep = NULL) {
       # Dropping format string
       by.line <- by.line[-1]
       
-      # Decompressing
-      by.line <- str_split(by.line, '((?<!^)[ \t]+)|(?=[a-zA-Z@%+-])')
-      by.line <- lapply(by.line, str_trim)
+      # First, convert all spaces to a single character width
+      by.line <- str_replace_all(by.line, '\\s+', ' ')
 
-      out <- decompress_asdf(by.line)
-      formatted <- data.frame(out$x, out$y)
+	  # Then substitute a space before all leading characters
+      by.line <- str_replace_all(by.line, '(?<![ ])([a-zA-Z@%+-])', ' \\1')
+
+	  # Check compression mode
+	  if ( any(str_detect(by.line, '[sS-Z]')) ) {
+        mode <- 3 # DUP
+      } else if ( any(str_detect, by.line, '[j-rJ-R%]') ) {
+ 	    mode <- 2 # DIF
+	  } else if ( any(str_detect, by.line, '[a-iA-I@]') ) {
+		mode <- 1 # SQZ
+	  } else {
+		mode <- 0
+	  }
+
+	  # If the data has encoded repeats, expand them
+	  if ( mode == 3 ) {
+	    
+	    # Pick off repeated elements
+        repeats <- unique(unlist(str_match_all(by.line, '[0-9a-zA-Z@%+-]+ [sS-Z][0-9]*')))
+ 
+		# Translate repeats
+	    patterns <- c('S'='1', 'T'='2', 'U'='3', 'V'='4', 'W'='5', 'X'='6', 'Y'='7', 'Z'='8', 's'='9')
+        replacements <- str_replace_all(repeats, patterns)
+
+	    # Split repeat pairs and apply the repeat
+		replacements <- str_split(replacements, ' ')
+		replacements <- unlist(lapply(replacements, function(x) paste(rep(x[1], as.numeric(x[2])), collapse = ' ')))
+
+		# Finally, re-insert the repeated values
+	    names(replacements) <- repeats
+	    by.line <- str_replace_all(by.line, replacements)
+	  }
+
+	  # Convert squeezed characters
+	  if ( mode >= 1) {
+		
+	    # Translate characters
+	    patterns <- c('A'='1', 'B'='2', 'C'='3', 'D'='4', 'E'='5', 'F'='6', 'G'='7', 'H'='8', 'I'='9',
+					  'a'='-1', 'b'='-2', 'c'='-3', 'd'='-4', 'e'='-5', 'f'='-6', 'g'='-7', 'h'='-8', 'i'='-9', '@'=0)
+
+		by.line <- str_replace_all(by.line, patterns)
+	  }
+
+	  # At this point, split by spaces
+      by.line <- lapply(by.line, str_trim)
+      by.line <- str_split(by.line, ' ')
+      
+	  # If the data is stored as differences, apply them now
+	  if ( mode >= 2 ) {
+
+	    # Translate characters
+	    patterns <- c('J'='1', 'K'='2', 'L'='3', 'M'='4', 'N'='5', 'O'='6', 'P'='7', 'Q'='8', 'R'='9',
+					  'j'='-1', 'k'='-2', 'l'='-3', 'm'='-4', 'n'='-5', 'o'='-6', 'p'='-7', 'q'='-8', 'r'='-9', '%'=0)
+
+		# Double check to make sure there are no non-differences present
+		all.differences <- lapply(by.line, function(x) all(str_detect(x[-(1:2)], '[j-rJ-R%]')))
+		msg <- 'Absolute values found among DIF compressed data, aborting.'
+	    if ( any(! unlist(all.differences)) ) stop(msg)
+
+		by.line <- lapply(by.line, str_replace_all, patterns)
+		
+		# Convert to numerical format and apply cumulative sum
+		by.line <- lapply(by.line, function(x) c(as.numeric(x[1]), cumsum(as.numeric(x)[-1])))
+
+		# With the difference comparison mode, the last value of each line should match the second value of following line
+		last.values <- unlist(lapply(by.line, function(x) `[`(x, length(x))))
+		second.values <- unlist(lapply(by.line, function(x) `[`(x, 2)))
+
+		n <- length(by.line)
+	 	msg <- 'y value data check failed in decompressing DIF data, aborting.'
+		if ( any(abs(last.values[1:(n-1)] - second.values[2:n]) > 1e-6) ) stop(msg)
+
+		# Stripping these last values
+		by.line[-n] <- lapply(by.line[-n], function(x) x[-length(x)])
+
+	  } else {
+		# Otherwise, just convert to numeric
+		by.line <- lapply(by.line, as.numeric)
+	  }
+
+	  # Picking off the x values
+	  x.values <- unlist(lapply(by.line, `[`, 1))
+
+	  # Counting observations per line
+	  counts <- unlist(lapply(by.line, function(x) length(x) - 1))
+
+	  # Ensuring that delta x remains consistent throughout
+	  n <- length(by.line)
+	  delta.x <- (x.values[2:n] - x.values[1:(n-1)])/counts[1:(n-1)]
+
+	  msg <- 'x value data check failed in data import, aborting.'
+	  if ( diff(range(delta.x))/mean(delta.x) > 1e-3 ) stop(msg)
+
+	  delta.x <- median(delta.x)
+
+	  n.out <- sum(counts)
+	  x.out <- seq(x.values[1], by = delta.x, length.out = n.out)
+	  y.out <- unlist(lapply(by.line, `[`, -1))
+
+      formatted <- data.frame(x.out, y.out)
       colnames(formatted) <- c(x.name, y.name)
     }
     else {
