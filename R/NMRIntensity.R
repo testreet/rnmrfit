735a27161a9d45a8a82ac77d5ebd2ccc2874150e
Stanislav Sokolenko <stanislav@sokolenko.net>
2019-06-04 19:10:43 -0300

    Reworked NMRIntensity.

--- R/NMRIntensity.R
+++ R/NMRIntensity.R
@@ -8,33 +8,71 @@
 #' Although 1D intensity can be represented by a single real/imaginary complex
 #' pair, there is no built-in representation for 2D (or higher dimension) data
 #' that may consist of real-real, real-imaginary, imaginary-real, and
-#' imaginary-imaginary components. This class serves as a simple container list
-#' that can be used to retrieve specific sets of values as required. Re() and
-#' Im() can be used to obtain pure real and pure imaginary compenents, whereas
-#' other specific real/imaginary can be are accesible via indexing. For 2D
-#' data, for example, $rr will output a numeric vector corresponding to the
-#' real-real data (equivalent to Re()).
-#' 
-#' @slot columns Defines the real/imaginary composition of each elemnt as a
-#'               series of "r" and "i" values.
+#' imaginary-imaginary components. This class approximates the extension of a
+#' complex values to multiple dimensions be defining a vector, where each
+#' element corresponds to a specific real-imaginary component. Re() and Im()
+#' functions can still be used to obtain pure real and pure imaginary
+#' compenents, whereas other specific real/imaginary can be are accesible with
+#' the $ function. For 2D data, for example, $rr will output a numeric vector
+#' corresponding to the real-real data (equivalent to Re()).
 #' 
 #' @name NMRIntensity-class
 #' @export
-NMRIntensity <- setClass("NMRIntensity", 
-                         slots = c(columns = "character"),
-                         contains = "list")
+NMRIntensity <- setClass("NMRIntensity", contains = "numeric")
 
 
-
-#==============================================================================>
-#  Constructor
-#==============================================================================>
 
 #------------------------------------------------------------------------------
-#' Generate an NMRIntensity object
+#' Container for NMR intensity data
 #' 
-#' Generates an NMRIntensity object from a series of vector, matrix, or
-#' data.frame object.
+#' Multiple NMRintensity vectors can be combined in an NMRIntensities list that
+#' supports the same Re(), Im() and $ functions as well as conversions to
+#' data.frames.
+#' 
+#' @name NMRIntensities-class
+#' @export
+NMRIntensities <- setClass("NMRIntensities", contains = "list")
+
+
+
+#==============================================================================>
+#  Constructors
+#==============================================================================>
+
+
+
+#------------------------------------------------------------------------------
+#' Shortcut for generating component combinations
+.complex_names <- function(n) {
+  names <- do.call(crossing, rep(list(c('r','i')), n))
+  rev(unlist(apply(names, 1, paste, collapse = '')))
+}
+
+
+
+#------------------------------------------------------------------------------
+#' Shortcut for generating ijk suffixes 
+.complex_suffixes <- function(n) {
+  names <- .complex_names(n)
+
+  # First convert 2nd i's and up
+  while ( n > 1 ) {
+    logic <- substr(names, n, n) == 'i'
+    substr(names, n, n)[logic] <- letters[8+n]
+    n = n - 1
+  }
+
+  # And then remove the r
+  gsub('r', '', names)
+}
+
+
+
+#------------------------------------------------------------------------------
+#' Generate an NMRIntensity or NMRIntensities object
+#' 
+#' Generates an NMRIntensity or plural NMRIntensities object from a series of
+#' vector, matrix, or data.frame inputs.
 #' 
 #' @param ... A collection of vector, matrix, or data.frame objects. Vectors
 #'            must be specified with names whereas the component names for
@@ -46,8 +84,18 @@ nmrintensity <- function(...) {
   args <- list(...)
   arg.names <- names(args)
 
-  # If there ar eno arguments, return null
+  # First, if there ar no arguments, return null
   if ( length(args) == 0 ) return(NULL)
+
+  # All arguments must have the same length or number of columns
+  n <- lapply(args, nrow)
+  n.alternate <- lapply(args, length)
+  logic <- unlist(lapply(n, is.null))
+  n[logic] <- n.alternate[logic]
+  n <- unique(unlist(n))
+
+  err <- 'All input objects must have the same length or row numbers'
+  if ( length(n) > 1 ) stop(err)
 
   # If there are no names, generate empty spaces to facilitate further parsing
   if ( length(arg.names) == 0 ) arg.names <- rep('', length(args))
@@ -89,29 +137,85 @@ nmrintensity <- function(...) {
   }
 
   # Combining and checking column names
-  combined <- do.call(cbind, parts)
-  columns <- colnames(combined)
+  new.terms <- do.call(cbind, parts)
+  new.columns <- colnames(new.terms)
+  n.char <- unique(nchar(new.columns))
+
+  err <- paste('All component names must have the same number of characters,',
+               'e.g. rr and ri vs r.')
+  if ( length(n.char) > 1 ) stop(err)
   
   err <- 'All component names must be composed of "r" and "i" characters.'
-  if (! all(grepl('^[ri]+$', columns)) ) stop(err)
+  if (! all(grepl('^[ri]+$', new.columns)) ) stop(err)
 
-  err <- 'All component names must be unique'
-  if ( length(columns) != length(unique(columns)) ) stop(err)
+  err <- 'All component names must be unique.'
+  if ( length(new.columns) != length(unique(new.columns)) ) stop(err)
 
-  # Sorting and splitting by row
-  columns <- rev(sort(columns))
+  # Extending
+  all.columns <- .complex_names(n.char)
 
-  combined <- as.matrix(combined[, columns])
-  combined <- split(combined, 1:nrow(combined))
+  all.terms <- matrix(0, nrow = n, ncol = 2^n.char)
+  colnames(all.terms) <- all.columns
 
-  # Outputting NMRIntensity object
-  new("NMRIntensity", combined, columns = columns)
+  # Inserting values
+  new.terms <- as.matrix(new.terms[, new.columns])
+  all.terms[, new.columns] <- new.terms
+
+  # Splitting by individual rows
+  all.terms <- split(all.terms, 1:n)
+
+  # Convert all individual rows to NMRIntensity
+  f <- function(x) new("NMRIntensity", x)
+  all.terms <- lapply(all.terms, f)
+
+  # If the final list length is greater than 1, make it plural
+  if ( length(all.terms) > 1) {
+    new("NMRIntensities", all.terms)
+  } else {
+    all.terms[[1]]
+  }
 }
 
 
 
 #==============================================================================>
-#  Basic setter and getter functions
+# Display methods
+#==============================================================================>
+
+
+
+#------------------------------------------------------------------------------
+#' @importFrom pillar type_sum
+#' @export
+type_sum.NMRIntensity <- function(x) {'ncmplx'}
+
+
+
+#------------------------------------------------------------------------------
+#' @importFrom pillar type_sum
+#' @export
+type_sum.NMRIntensities <- function(x) {'ncmplx'}
+
+
+#------------------------------------------------------------------------------
+#' Show an NMRIntensity object
+#' 
+#' A concise display for NMRIntensity.
+#' 
+#' @param object An NMRIntensity object.
+#' 
+#' @export
+setMethod("show", "NMRIntensities", 
+  function(object) {
+    
+    cat('An object of NMRIntensities class\n\n')
+    print(summary(as.data.frame(object)))
+  })
+
+
+
+#==============================================================================>
+#  Basic getter functions for NMRIntensity
 #==============================================================================>
 
 
@@ -119,40 +223,28 @@ nmrintensity <- function(...) {
 #------------------------------------------------------------------------
 #' Get the pure real component of the data
 #' 
-#' Retrieves either the pure real component or the component with the most real
-#' terms when dealing with segmented data.
+#' Retrieves the pure real component of NMRIntensity.
 #' 
 #' @param z NMRIntensity data.
 #' 
 #' @return A numeric vector.
 #' 
 #' @export
-setMethod("Re", "NMRIntensity", 
-  function(z) {
-    out <- unlist(lapply(z, function(x) {x[1]}))
-    names(out) <- NULL
-    out
-  })
+setMethod("Re", "NMRIntensity", function(z) z[1]) 
 
 
 
 #------------------------------------------------------------------------
 #' Get the pure imaginary component of the data
 #' 
-#' Retrieves either the pure imaginary component or the component with the most
-#' imaginary terms when dealing with segmented data.
+#' Retrieves the pure imaginary component of NMRIntensity.
 #' 
 #' @param z NMRIntensity data.
 #' 
 #' @return A numeric vector.
 #' 
 #' @export
-setMethod("Im", "NMRIntensity", 
-  function(z) {
-    out <- unlist(lapply(z, function(x) {x[length(x)]}))
-    names(out) <- NULL
-    out
-  })
+setMethod("Im", "NMRIntensity", function(z) z[length(z)])
 
 
 
@@ -167,46 +259,124 @@ setMethod("Im", "NMRIntensity",
 #' @param name A character name for the component. This can be an exact match
 #'             such as "ri" or a partial match such as "r.".
 #' 
-#' @return A numeric vector if only a single component is matched, a complex
-#'         vector if two components are matched, and an NMRIntensity object if
-#'         three or more components are matched.
+#' @return A complex vector if two components are matched, a numeric vector
+#'         otherwise.
 #' 
 #' @export
 `$.NMRIntensity` <- function(x, name) {
 
-  # First, check if name is one of the columns
-  if ( name %in% x@columns ) {
-    i <- which(name == x@columns)
-    out <- lapply(x, function(x) {x[i]})
-    names(out) <- NULL
-    unlist(out)
-  }
-  # Otherwise try and match
-  else {
-    i <- grep(name, x@columns)
+  err <- 'The total number of characters must correspond to components of x.'
+  if ( nchar(name) != sqrt(length(x)) ) stop(err)
 
-    # Output depends on how many matches there are
-    if ( length(i) == 0 ) {
-      NULL
-    }
-    else if ( length(i) == 1 ) {
-      out <- lapply(x, function(x) {x[i]})
-      names(out) <- NULL
-      unlist(out)
-    }
-    else if ( length(i) == 2 ) {
-      re = unlist(lapply(x, function(x) {x[i[1]]}))
-      im = unlist(lapply(x, function(x) {x[i[2]]}))
-      out <- complex(re = re, im = im)
-      names(out) <- NULL
-      out
-    }
-    else {
-      # First filter the list
-      d.list <- lapply(x, function(x) {x[i]})
-      d.mat <- do.call(rbind, d.list)
-      colnames(d.mat) <- x@columns
-      nmrintensity(d.mat)
-    }
+  i <- grep(name, .complex_names(sqrt(length(x))))
+
+  # Output depends on how many matches there are
+  if ( length(i) == 0 ) {
+    NULL
+  }
+  else if ( length(i) == 2 ) {
+    re =x[i[1]]
+    im = x[i[2]]
+    complex(re = re, im = im)
+  }
+  else {
+    x[i]
+  }
+}
+
+
+
+#==============================================================================>
+#  Basic getter functions for NMRIntensities
+#==============================================================================>
+
+
+
+#------------------------------------------------------------------------
+#' Get the pure real component of the data
+#' 
+#' Retrieves the pure real component of NMRIntensities.
+#' 
+#' @param z NMRIntensities data.
+#' 
+#' @return A numeric vector.
+#' 
+#' @export
+setMethod("Re", "NMRIntensities", function(z) unlist(lapply(z, Re))) 
+
+
+
+#------------------------------------------------------------------------
+#' Get the pure imaginary component of the data
+#' 
+#' Retrieves the pure imaginary component of NMRIntensities.
+#' 
+#' @param z NMRIntensities data.
+#' 
+#' @return A numeric vector.
+#' 
+#' @export
+setMethod("Im", "NMRIntensities", function(z) unlist(lapply(z, Im)))
+
+
+
+#------------------------------------------------------------------------
+#' Convert NMRIntensities object into a full data.frame
+#' 
+#' Since an NMRIntensities object is essentially a data.frame broken down by
+#' rows so that it can used as a column inside other data.frames, it's
+#' sometimes convenient to access the data as a proper data.frame object.
+#' 
+#' @param x NMRIntensities object.
+#' 
+#' @return A data.frame with columns labelled as real/imaginary components.
+#' 
+#' @export
+as.data.frame.NMRIntensities <- function(x) {
+
+  d <- do.call(rbind, x)
+  colnames(d) <- .complex_names(sqrt(ncol(d)))
+  d
+
+}
+
+
+
+#------------------------------------------------------------------------
+#' Get a specific real/imaginary component
+#' 
+#' Specify the components of interest using a series of "r" and "i" characters,
+#' such as "ri" or "ir". Internally, the name is passed into grep() so "r.",
+#' for example, will be matched as both "rr" and "ri".
+#' 
+#' @param x NMRIntensities object.
+#' @param name A character name for the component. This can be an exact match
+#'             such as "ri" or a partial match such as "r.".
+#' 
+#' @return A numeric vectors if a single component is matched, a complex vector
+#'         if two components are matched, and a data.frame otherwise.
+#' 
+#' @export
+`$.NMRIntensities` <- function(x, name) {
+
+  err <- 'The total number of characters must correspond to components of x.'
+  if ( nchar(name) != sqrt(length(x[[1]])) ) stop(err)
+
+  i <- grep(name, .complex_names(length(x)))
+
+  # Output depends on how many matches there are
+  if ( length(i) == 0 ) {
+    NULL
+  }
+  else if ( length(i) == 1 ) {
+    unlist(lapply(x, `[`, i))
+  }
+  else if ( length(i) == 2 ) {
+    re = unlist(lapply(x, `[`, i[1]))
+    im = unlist(lapply(x, `[`, i[2]))
+    complex(re = re, im = im)
+  }
+  else {
+    as.data.frame(x)[,i]
   }
 }
